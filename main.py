8294f64aeaf97552a322aa4f7fb375567969f2bd
Chris Johnson <crjohns@mit.edu>
2012-08-26 21:58:45 -0400

    Started new project using curses and python

---
+++ main.py
@@ -0,0 +1,79 @@
+import curses
+
+PROGRAM_NAME = "The Handler"
+VERSION = "0.0 dev"
+
+
+def newfullwin():
+    ret = curses.newwin(24, 80, 0, 0)
+    ret.nodelay(1)
+    return ret
+    
+
+
+class GameLooper:
+
+    def __init__(self):
+        pass
+
+    def event(self, window):
+        char = window.getch()
+        if char == -1:
+            return False
+        else:
+            if chr(char) == 'q' or chr(char) == 'Q':
+                exit(0)
+            return char
+
+    def relax(self):
+        curses.napms(20)
+
+
+
+
+
+class Menu:
+
+    def __init__(self, title=None, options=[], actions=[]):
+        self._title = title
+        self._options = options
+        self._actions = actions
+
+    def run(self, game):
+        while True:
+            char = game.event(self._window)
+            game.relax()
+
+    def start(self, game):
+        self._window = newfullwin()
+        
+        if self._title:
+            self._window.addstr(0, 40-len(self._title)/2, self._title)
+
+        line = 5
+        letter = ord('A')
+        for option in self._options:
+            self._window.addstr(line, 0, chr(letter) + ' - ' + option)
+            line += 1
+            letter += 1
+
+        self._window.refresh()
+
+        self.run(game)
+
+
+def startup(screen):
+    curses.curs_set(0)
+
+    game = GameLooper()
+
+
+    menu = Menu(PROGRAM_NAME + " " + VERSION,
+            ["New Game", "Load Game", "Statistics"], [None, None, None])
+
+
+
+    menu.start(game)
+
+
+curses.wrapper(startup)
