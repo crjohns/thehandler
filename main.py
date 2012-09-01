59089d18cf3848051f2ec19aebe48545fa2f2bc2
Chris Johnson <crjohns@mit.edu>
2012-09-01 15:50:28 -0400

    Removed old curses stuff and switched to pygcurse

--- main.py
+++ main.py
@@ -1,88 +1,19 @@
-import curses
+from pygcurse import *
+import bisect
 
 PROGRAM_NAME = "The Handler"
 VERSION = "0.0 dev"
 
+window = new PygcurseWindow(80,25, PROGRAM_NAME + " " + VERSION, fullscreen=False)
 
-def newfullwin():
-    ret = curses.newwin(24, 80, 0, 0)
-    ret.nodelay(1)
-    return ret
+
+class Game:
     
+    def __init__():
+        self.windows = []
+        self.windowcmp = lambda x,y: x[0] < y[0]
 
-
-class GameLooper:
-
-    def __init__(self):
-        pass
-
-    def event(self, window):
-        char = window.getch()
-        if(char == -1):
-            return char
-        if chr(char) == 'q' or chr(char) == 'Q':
-            exit(0)
-        return char
-
-    def relax(self):
-        curses.napms(20)
-
-
-
-
-
-class Menu:
-
-    def __init__(self, title=None, options=[], actions=[]):
-        self._title = title
-        self._options = options
-        self._actions = actions
-
-    def run(self, game):
-        while True:
-            char = game.event(self._window)
-            offset = char - ord('A')
-            if not offset in range(0, len(self._actions)):
-                offset = char - ord('a')
-
-            if offset in range(0, len(self._actions)):
-                if not self._actions[offset]():
-                    return
-            
-            game.relax()
-
-    def start(self, game):
-        self._window = newfullwin()
-        
-        if self._title:
-            self._window.addstr(0, 40-len(self._title)/2, self._title)
-
-        line = 5
-        letter = ord('A')
-        for option in self._options:
-            self._window.addstr(line, 0, chr(letter) + ' - ' + option)
-            line += 1
-            letter += 1
-
-        self._window.refresh()
-
-        self.run(game)
-
-
-def startup(screen):
-    curses.curs_set(0)
-
-    game = GameLooper()
-
-
-    false = lambda: False
-
-    menu = Menu(PROGRAM_NAME + " " + VERSION,
-            ["New Game", "Load Game", "Statistics"], [false, None, None])
-
-
-
-    menu.start(game)
+    def
 
 
 curses.wrapper(startup)
