ad009bdd7d8e616101ffb5d31ba1ba2b965201c0
Christopher R. Johnson <crjohns@csail.mit.edu>
2013-01-13 18:55:05 -0500

    Integrated sqlalchemy

--- thehandler/model.py
+++ thehandler/model.py
@@ -1,14 +1,36 @@
 import thehandler
 import json
 import random
-
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 names = None
+
+engine = None
 
 def loadNames():
     global names
     f = open('data/names.json').read()
     names = json.loads(f)
+
+def saveNames(session):
+    if not names:
+        loadNames()
+
+    addus = []
+
+    for entry in names:
+        addus.append( Name( name = entry['name'], \
+                            gender = entry['gender'], \
+                            part = entry['part'], \
+                            nation = entry['nation']))
+
+    session.add_all(addus)
+    session.commit()
+
 
 
 def getName(gender):
@@ -22,6 +44,38 @@ def getName(gender):
     last = random.sample(anylast,1)[0]['name'].capitalize()
 
     return (str(first), str(last))
+
+
+class Name(Base):
+    __tablename__ = 'randnames'
+    id = Column(Integer, primary_key = True)
+    name = Column(String, index = True)
+    gender = Column(String, index = True)
+    part = Column(String, index = True)
+    nation = Column(String, index = True)
+
+    def __init__(self, name, gender, part, nation):
+        self.name = name
+        self.gender = gender
+        self.part = part
+        self.nation = nation
+
+    def __repr__(self):
+        return "(Name '%s', %s, %s, %s)" % (self.name, self.gender, self.part, self.nation)
+
+def openDB(gamename, newgame=False, debug=False):
+    global engine 
+    global Base
+
+    dbname = 'sqlite:///%s.db' % gamename
+    engine = create_engine(dbname, echo=debug)
+
+    if newgame:
+        Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind = engine)
+    
+    return Session()
 
 
 
